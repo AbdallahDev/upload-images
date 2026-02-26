a4692ad9122dcbdc2fc46fc2ab091b84221ed490
AbdallahDev <abdallah.dev85@gmail.com>
2026-02-26 22:11:39 +0300

    stage

---
+++ data.js
@@ -0,0 +1,14 @@
+export const urls = [
+    "https://4kwallpapers.com/images/walls/thumbs_3t/3984.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/22177.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/22476.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/25476.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/12476.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/22976.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/22472.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/22473.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/22474.jpg",
+    "https://4kwallpapers.com/images/walls/thumbs_3t/22475.jpg",
+]
+
+export default urls;
--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+import urls from './data.js'
+
 const containerEl = document.getElementById('container')
 let containerHtml = ""
-const imagesAreaEl = document.getElementById('imagesArea')
-let imagesAreaHtml = ""
-img_urls = [
-    "https://4kwallpapers.com/images/walls/thumbs_3t/24069.png",
-    "https://4kwallpapers.com/images/walls/thumbs_3t/3984.jpg",
-    "https://4kwallpapers.com/images/walls/thumbs_3t/25476.jpg",
-    "https://4kwallpapers.com/images/walls/thumbs_3t/22476.jpg",
-    "https://4kwallpapers.com/images/walls/thumbs_3t/22476.jpg",
-    "https://4kwallpapers.com/images/walls/thumbs_3t/22476.jpg",
-]
 
 function createUploadMenu(urls) {
+    const uploadSectionEl = document.createElement('div')
+    uploadSectionEl.id = "uploadSection"
+
+    const titleEl = document.createElement("div")
+    titleEl.id = "title"
+    titleEl.textContent = "Select Images to Upload"
+    uploadSectionEl.innerHTML = titleEl.outerHTML
+
+    const uploadBtnEl = document.createElement("button")
+    uploadBtnEl.id = "uploadBtn"
+    uploadBtnEl.textContent = "Upload"
+
+    const imagesAreaEl = document.createElement('div')
+    imagesAreaEl.id = "imagesArea"
+    let imagesAreaHtml = ""
     urls.forEach(url => {
         const fileName = url.match(/[^/]+$/)[0]
         imagesAreaHtml += `
@@ -22,14 +29,51 @@ function createUploadMenu(urls) {
         `
     });
     imagesAreaEl.innerHTML += imagesAreaHtml
+    uploadSectionEl.innerHTML += imagesAreaEl.outerHTML
+
+    uploadSectionEl.appendChild(uploadBtnEl)
+    uploadBtnEl.addEventListener('click', () => renderImages(urls))
+
+
+    containerEl.appendChild(uploadSectionEl)
 }
 
-function renderImages(urls) {
+async function renderImages(urls) {
     containerHtml = ""
     containerEl.innerHTML = containerHtml
+    containerEl.style.backgroundColor = "white"
+
+    const loadingEl = document.createElement('h1')
+    loadingEl.textContent = "Loading images, please wait......"
+    containerEl.innerHTML += loadingEl.outerHTML
+
+    try {
+        const imageViewerEl = document.createElement('div')
+        imageViewerEl.id = "imageViewer"
+
+        const images = urls.map((url) => getImage(url))
+        const results = await Promise.all(images)
+        results.forEach((img) => {
+            imageViewerEl.innerHTML += img.outerHTML
+        })
+
+        containerHtml += imageViewerEl.outerHTML
+        containerEl.innerHTML = containerHtml
+    } catch (error) {
+        console.log(error)
+        containerEl.innerHTML = `<h1 id='error'>Something wrong, Images failed to render.<br>Please try again.</h1>`
+    }
 }
 
-createUploadMenu(img_urls)
-// renderImages()
+function getImage(url) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const img = new Image()
+            img.src = url
+            img.addEventListener('load', () => resolve(img))
+            img.addEventListener('error', () => reject(new Error))
+        }, 5000)
+    })
+}
 
-
+createUploadMenu(urls)
